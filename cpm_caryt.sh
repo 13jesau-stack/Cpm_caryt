8cbe5ad772cc0f91201e585c1684d0efa0ad646a
13jesau-stack <13jesau@gmail.com>
2025-11-08 14:37:53 -0600

    Actualizar cpm_caryt.sh
    nuevo

--- cpm_caryt.sh
+++ cpm_caryt.sh
@@ -1,4 +1,3 @@
-cat > cpm_caryt.sh <<'EOF'
 #!/bin/bash
 clear
 blue='\033[1;34m'
@@ -7,21 +6,21 @@ yellow='\033[1;33m'
 green='\033[1;32m'
 reset='\033[0m'
 
-# Marco y título
 echo -e "${blue}╔═══════════════════════════════════════╗"
-echo -e "║        🌟 CPM CARYT 🌟                 ║"
-echo -e "║         Canal/Compartir YouTube       ║"
+echo -e "║        🌟 CPM CARYT APP 🌟             ║"
+echo -e "║       Menú Termux con apps             ║"
 echo -e "╚═══════════════════════════════════════╝${reset}"
 echo
 
-# Opciones del menú
 echo -e "${cyan}1) Ejecutar cpm1.py"
 echo -e "2) Ejecutar cpm2.py"
 echo -e "3) YouTube"
-echo -e "4) Salir${reset}"
+echo -e "4) Abrir app"
+echo -e "5)espacio virtual"
+echo -e "6) Salir${reset}"
 echo
 
-read -p "👉 Elige una opción (1-4): " opcion
+read -p "👉 Elige una opción (1-6): " opcion
 
 case $opcion in
   1)
@@ -33,10 +32,20 @@ case $opcion in
     python cpm2.py
     ;;
   3)
-    echo -e "${green}Abriendo YouTube para compartir...${reset}"
+    echo -e "${green}Abriendo YouTube...${reset}"
     termux-open-url "https://youtube.com/@caryt.305?si=yzom3bnFIL8ToDCs"
     ;;
   4)
+    echo -e "${green}Abriendo app
+...${reset}"
+    termux-open-url "https://www.mediafire.com/file/48yfn1j4a2pxj0f/CPM_Hacks_1.0.apk/file"
+    ;;
+  5)
+    echo -e "${green}abriendo espacio
+...${reset}"
+termux-open-url "https://www.mediafire.com/file/w5718poeoldch8i/LuluBoxSuper_2.0.9.apk/file"
+    ;;
+  6)
     echo -e "${cyan}Saliendo...${reset}"
     exit 0
     ;;
@@ -44,4 +53,3 @@ case $opcion in
     echo -e "${yellow}Opción no válida${reset}"
     ;;
 esac
-EOF
